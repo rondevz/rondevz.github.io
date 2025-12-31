e07032fc5c1e7806bdd9c47d4c002f51c78fb852
Ronald Pereira <ronaldpereira01@gmail.com>
2025-12-31 11:54:05 -0400

    update baseHead

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "rondevz";
+export const SITE_TITLE = "Ronald Pereira";
 export const SITE_DESCRIPTION =
-	"Welcome to my website! I am a computer engineer and a software developer that loves code, ganming and music.";
+	"Computer Engineer. Senior Software Developer. Puzzles Solver with Software. Let's make software that matters.";
+
+export const SITE_IMAGE = "/metadata-image.jpg";
